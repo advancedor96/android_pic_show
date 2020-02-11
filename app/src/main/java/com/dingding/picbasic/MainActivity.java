060625ebc115ba6e56933186b2b7795c49c7a1d1
丁丁 <dingding@bxb.tw>
2020-02-11 17:45:03 +0800

    正版

--- app/src/main/java/com/dingding/picbasic/MainActivity.java
+++ app/src/main/java/com/dingding/picbasic/MainActivity.java
@@ -1,41 +1,48 @@
 package com.dingding.picbasic;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import com.BoardiesITSolutions.FileDirectoryPicker.DirectoryPicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.orhanobut.logger.Logger;
 import com.yarolegovich.lovelydialog.LovelyTextInputDialog;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.documentfile.provider.DocumentFile;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    int READ_REQUEST_CODE = 1;
-    ImageView iv_f;
+    private static final int REQUEST_DIRECTORY_PICKER = 2;
     RecyclerView rv;
     ArrayList<MyImage> images = new ArrayList<>();
     ImageAdapter adapter;
     GlobalApp dapp;
     int num_pic = 5;
+    int now_idx = 0;
+    File[] allfiles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         dapp = (GlobalApp) getApplicationContext();
         rv = findViewById(R.id.rv);
@@ -45,61 +52,87 @@ public class MainActivity extends AppCompatActivity {
         rv.setAdapter(adapter);
         rv.setLayoutManager(new LinearLayoutManager(this));
 
-        String str_uri = dapp.pref.getString("folderUri","");
-        if(str_uri!=""){
-            showData(Uri.parse(str_uri));
-            Toast.makeText(this, "載入上次資料夾", Toast.LENGTH_SHORT).show();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNext(now_idx);
+            }
+        });
+
+
+        String path = dapp.pref.getString("path","");
+        if(path!=""){
+            now_idx = new Random().nextInt((new File(path)).listFiles().length);
+            Logger.d("設為"+now_idx);
+            showData(path);
+            Toast.makeText(this, String.format("載入%d張，從%s", num_pic, path), Toast.LENGTH_LONG).show();
         }else{
             Toast.makeText(this, "先指定資料夾", Toast.LENGTH_SHORT).show();
         }
 
     }
-    public void showData(Uri treeUri){
+    public void showNext(int idx){
         images.clear();
-
-        DocumentFile pickedDir = DocumentFile.fromTreeUri(this, treeUri);
-        Toast.makeText(this, "數量：" + pickedDir.listFiles().length, Toast.LENGTH_SHORT).show();
-        DocumentFile[] fs = pickedDir.listFiles();
-
-
-        for(int i=0;i<fs.length;i++){
-            if(i>=num_pic) break;
-            images.add(new MyImage(fs[i].getUri(), fs[i].getName()));
+        Toast.makeText(MainActivity.this, "打開" + idx, Toast.LENGTH_LONG).show();
+        for(int i=idx;i<allfiles.length;i++){
+            Logger.d("yo"+i);
+            if(i - idx >=num_pic) {
+                now_idx = i;
+                break;
+            }
+            images.add(new MyImage(Uri.fromFile(allfiles[i]), allfiles[i].getName()));
         }
-
-        adapter.notifyDataSetChanged();
-    }
-    private void pickRandom20() {
-        images.clear();
-
-        String str_uri = dapp.pref.getString("folderUri","");
-        Uri treeUri = Uri.parse(str_uri);
-
-        DocumentFile pickedDir = DocumentFile.fromTreeUri(this, treeUri);
-        DocumentFile[] fs = pickedDir.listFiles();
-
-        Random rand = new Random();
-        int random_idx = rand.nextInt(fs.length); // Obtain a number between [0 - 49].
-
-
-        Toast.makeText(this, "編號：" + random_idx, Toast.LENGTH_SHORT).show();
-
-
-        for(int i=random_idx;i<fs.length;i++){
-            if(i - random_idx >= num_pic) break;
-            images.add(new MyImage(fs[i].getUri(), fs[i].getName()));
-        }
-
         adapter.notifyDataSetChanged();
         rv.scrollToPosition(0);
+    }
+    public void showData(String path){
+        images.clear();
+        File pickedDir = new File(path);
+        Toast.makeText(this, "總數：" + pickedDir.listFiles().length, Toast.LENGTH_SHORT).show();
+        allfiles = pickedDir.listFiles();
+        Logger.d("排序開始");
+        Arrays.sort(allfiles, new Comparator<File>() {
+            public int compare(File f1, File f2) {
+                long diff = f1.lastModified() - f2.lastModified();
+                if (diff > 0)
+                    return 1;
+                else if (diff == 0)
+                    return 0;
+                else
+                    return -1;
+            }
+            public boolean equals(Object obj) {
+                return true;
+            }
+
+        });
+
+        for(int i=now_idx;i<allfiles.length;i++){
+            if(i - now_idx>=num_pic) {
+                now_idx = i;
+                break;
+            }
+            images.add(new MyImage(Uri.fromFile(allfiles[i]), allfiles[i].getName()));
+        }
+        adapter.notifyDataSetChanged();
+        rv.scrollToPosition(0);
+    }
+
+    public void pickFolder(){
+        Intent intent = new Intent(this, DirectoryPicker.class);
+        startActivityForResult(intent, REQUEST_DIRECTORY_PICKER);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == READ_REQUEST_CODE){
-            Uri treeUri = data.getData();
-            dapp.pref.edit().putString("folderUri", treeUri.toString()).commit();
-            showData(treeUri);
+        if (requestCode == REQUEST_DIRECTORY_PICKER){
+            if(resultCode == Activity.RESULT_OK){
+                String currentPath = data.getStringExtra(DirectoryPicker.BUNDLE_CHOSEN_DIRECTORY);
+                dapp.pref.edit().putString("path", currentPath).commit();
+                showData(currentPath);
+            }
         }
     }
 
@@ -112,12 +145,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.select_folder) {
-            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-            intent.addCategory(Intent.CATEGORY_DEFAULT);
-            startActivityForResult(intent, READ_REQUEST_CODE);
+            pickFolder();
             return true;
         }else if (item.getItemId() == R.id.random) {
-            pickRandom20();
+            int random_idx = new Random().nextInt(allfiles.length);
+            showNext(random_idx);
             return true;
         }else if (item.getItemId() == R.id.setting) {
             new LovelyTextInputDialog(this)
@@ -135,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
                             dapp.pref.edit().putString("num_pic", text).commit();
                             num_pic = Integer.parseInt(dapp.pref.getString("num_pic", ""));
+                            showNext(now_idx);
                         }
                     })
                     .setNegativeButton(android.R.string.no, null)
